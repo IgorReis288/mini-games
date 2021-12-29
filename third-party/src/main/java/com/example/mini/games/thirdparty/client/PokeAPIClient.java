de3cfaf1b1f89bdd4201e4877bbbc5e546317a62
IgorReis288 <sinistro.oliveira@gmail.com>
2021-12-29 12:24:04 -0300

    start project

---
+++ third-party/src/main/java/com/example/mini/games/thirdparty/client/PokeAPIClient.java
@@ -0,0 +1,11 @@
+package com.example.mini.games.thirdparty.client;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PokeAPIClient {
+
+    public void getTypes(){
+        
+    }
+}
